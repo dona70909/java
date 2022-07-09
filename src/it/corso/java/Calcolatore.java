2422bf438b01a4c1afe6acf924fa23262f006c10
Donatella7 <campaniellodonatella@gmail.com>
2022-07-09 20:04:21 +0200

    first commit

---
+++ src/it/corso/java/Calcolatore.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package it.corso.java;
+
+/**
+ * @author Donatella
+ *
+ */
+public class Calcolatore {
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		double a = 10;
+		
+		double b = 20;
+		
+		Calcolatrice c = new Calcolatrice();
+		
+		System.out.println(c.somma(a, b));
+		System.out.println(c.sottrazione(a, b));
+		System.out.println(c.divisione(a, b));
+		System.out.println(c.moltiplicazione(a, b));
+
+	}
+
+}
---
+++ src/it/corso/java/Calcolatrice.java
@@ -0,0 +1,27 @@
+package it.corso.java;
+
+//prova commit eclipseeeeeeeeee
+
+public class Calcolatrice {
+	
+	public double somma (double a, double b) {
+		
+		return a + b;
+	}
+	
+	public double sottrazione (double a, double b) {
+			
+			return a - b;
+		}
+	
+	public double moltiplicazione (double a, double b) {
+		
+		return a * b;
+	}
+	
+	public double divisione (double a, double b) {
+		
+		return a / b;
+	}
+
+}
